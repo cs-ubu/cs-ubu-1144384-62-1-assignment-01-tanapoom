c7a46a78cae302166bb439057fcba5a4a1683439
tanapoom <thanaphum.su.57@ubu.ac.th>
2019-07-05 15:40:54 +0700

    i am done

---
+++ app01/index.js
@@ -0,0 +1,34 @@
+//import * as request from 'request'
+//import * as readline from 'readline'
+const request = require('request')
+const readline = require('readline')
+
+const rl = readline.createInterface({input: process.stdin, output: process.stdout})
+
+function showExchange(rates) {
+  rl.question('กรอกจำนวนเงิน (บาท) ', (answer) => {
+    const money = parseFloat(answer)
+    console.log(`ผู้ใช้มีเงิน ${money} บาท`)
+    for (let k in rates) {
+      console.log(`\t: ${k} ${(money*rates[k]/rates['THB']).toFixed(2)}`)
+    }
+    rl.close()
+  })
+}
+
+const url = `https://api.exchangeratesapi.io/latest`
+
+let result = '';
+request
+    .get(url)
+    .on('response', res => {
+            console.log(`response statusCode : ${res.statusCode}`)
+            // console.log(res.headers)
+        })
+    .on('data', data => {
+      result += data
+        })
+    .on('end', () => {
+      const exchange = JSON.parse(result)
+      showExchange(exchange.rates)
+        })
